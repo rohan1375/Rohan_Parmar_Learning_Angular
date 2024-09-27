e138c5c07b0a383395438ea89ba7f7862bfe5266
DELL <w0845024@myscc.ca>
2024-09-27 01:54:57 -0400

    Assignment-3 completed all steps completed

--- src/app/marvel-hero-list-item/marvel-hero-list-item.component.ts
+++ src/app/marvel-hero-list-item/marvel-hero-list-item.component.ts
@@ -1,12 +1,16 @@
 import {Component, Input} from '@angular/core';
-
+import {User} from"../Shared/Model/User";
+import {NgForOf, NgIf} from "@angular/common";
 @Component({
   selector: 'app-marvel-hero-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './marvel-hero-list-item.component.html',
   styleUrl: './marvel-hero-list-item.component.css'
 })
 export class MarvelHeroListItemComponent {
-@Input() MarvelComics:any;
+@Input() MarvelComics?: User;
 }
--- src/app/marvel-hero-list/marvel-hero-list.component.ts
+++ src/app/marvel-hero-list/marvel-hero-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import {User} from "../Shared/Model/User";
 import {MarvelHeroListItemComponent} from "../marvel-hero-list-item/marvel-hero-list-item.component";
+import {NgClass, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-marvel-hero-list',
   standalone: true,
   imports: [
-    MarvelHeroListItemComponent
+    MarvelHeroListItemComponent,
+    NgClass,
+    NgForOf
   ],
   templateUrl: './marvel-hero-list.component.html',
   styleUrl: './marvel-hero-list.component.css'
