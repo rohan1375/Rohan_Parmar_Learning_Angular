21ea6ad4e949e3a10b1cbab821ddd361b9c8a9a1
DELL <w0845024@myscc.ca>
2024-10-02 19:27:01 -0400

    Assignment-4 step 4-5 completed

---
+++ src/app/Shared/mockUser.ts
@@ -0,0 +1,10 @@
+import {User} from "./Model/User";
+
+export  const MarvelComics: User[]=[
+  {CharacterName: "Thor", power: "God of Thunder", age: 1500, weapons: "Mjolnir", iscanfly: true},
+  {CharacterName: "Iron-Man", power: "Iron-Armor", age: 40, weapons: "Arc-reactor", iscanfly: true},
+  {CharacterName: "Captain America", power: "Super human strength", age: 96, weapons: "Vibranium Shield", iscanfly: false},
+  {CharacterName: "Wolverine", power: "Healing", age: 200, weapons: "Adamantium Claws", iscanfly: false},
+  {CharacterName: "Doctor Strange", power: "Magic", age: 45, weapons: "Eye of Agamotto, Cloak", iscanfly: true},
+  {CharacterName: "Spider-Man", power: "Spider-Sense, Strength", age: 20, weapons: "Web-Shooters", iscanfly: false}
+]
