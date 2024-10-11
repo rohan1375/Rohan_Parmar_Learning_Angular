b34c571a67ef6632b397fcd5145bd09874a7c54b
DELL <w0845024@myscc.ca>
2024-10-11 14:43:02 -0400

    Step 4 and 5 completed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {MarvelHeroListComponent} from "./app/marvel-hero-list/marvel-hero-list.component";
+import {MarvelHeroListItemComponent} from "./app/marvel-hero-list-item/marvel-hero-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path:'', redirectTo: '/MarvelComics', pathMatch: 'full'},
+  { path: 'MarvelComics', component: MarvelHeroListComponent },
+  { path: 'MarvelComics/:CharacterName', component: MarvelHeroListItemComponent },
+]
+
+bootstrapApplication(AppComponent,{ providers:[provideRouter(routes)]})
+.then(r=>console.log('successful'));
